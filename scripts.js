7df4d0e131f1035dd5f9d1cabc39211977be0c3b
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-04-27 23:46:06 -0300

    dia 27 de abril 2023

--- scripts.js
+++
@@ -1,4 +0,0 @@
-const myName = "Thaissa Barbosa";
-const hello = `Hellow, ${myName}, esse é seu repositório privado de estudosssssss!!!`;
-
-console.loh(hello);
--- test.ts
+++ test.ts
@@ -1,3 +1,5 @@
-const test: string = 'Testan aqui e paea não prder a hgdhgsd';
-
-console.log(test)
+const string: string = "Testan aqui e paea não prder a hgdhgsd";
+const num: number = 20;
+const boolean: boolean = true;
+const arrayS: string[] = ["Testan", "aqui", "rder"];
+const numArray: number[] = [1, 2, 2, 5];
