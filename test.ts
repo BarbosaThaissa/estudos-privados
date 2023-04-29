ff2894902fe43093cb88c293b12e31db43ae118e
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-04-28 21:59:35 -0300

    myFunction

--- test.ts
+++ test.ts
@@ -3,3 +3,9 @@ const num: number = 20;
 const boolean: boolean = true;
 const arrayS: string[] = ["Testan", "aqui", "rder"];
 const numArray: number[] = [1, 2, 2, 5];
+
+const myFunction = (nume: number) => {
+   return nume * 19;
+}
+
+console.log(myFunction(num));
