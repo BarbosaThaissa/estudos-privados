fbf7ec9cdb02f70435f1591180e1efc57b274698
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-05-21 20:43:06 -0300

    fixt: ajeitando arquivo main ts

--- main.ts
+++ main.ts
@@ -1,46 +1,3 @@
-let testeTs: number = 5;
-
-interface ObjetoNumStris {
-  nome: string;
-  preco: number;
-}
-interface Pedidosg {
-  cliente: string;
-  endereco: string;
-  itens: ObjetoNumStris[];
-  taxaEntrega: number;
-}
-
-const pedidoTs: Pedidosg = {
-  cliente: "Joao",
-  endereco: "Rua B, 456",
-  itens: [
-    { nome: "Hamburuer", preco: 15 },
-    { nome: "Batata frita", preco: 8 },
-  ],
-  taxaEntrega: 5.0,
-};
-
-const pedidosTs = (
-  client: string,
-  end: string,
-  qtdItens: number,
-  iten: ObjetoNumStris[]
-) => {
-  let result = iten.map((sum) => {
-    testeTs += sum.preco;
-  });
-
-  return `Pedido: ${client} \n Endereco de entrega: ${end} \n Total: R$ ${testeTs.toFixed(
-    2
-  )}`;
-};
-
-console.log(
-  "EX 1, arquivo TypeScript \n \n",
-  pedidosTs(pedidoTs.cliente, pedidoTs.endereco, 2, pedidoTs.itens)
-);
-
 //EXERCIO 2
 interface ObjetoClientEx2 {
   nome: string;
@@ -81,40 +38,6 @@ console.log(
   )
 );
 
-// EXERCIO 3
-interface ObejEx3 {
-  nome: string;
-  idade: number;
-}
-
-const objeto11: ObejEx3 = { nome: "João", idade: 25 };
-const objeto22: ObejEx3 = { nome: "Maria", idade: 30 };
-
-function compararObjetosTs(obj1: ObejEx3, obj2: ObejEx3): string {
-  // Verifica se os objetos têm a mesma quantidade de propriedades
-  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-    return "Os objetos são diferentes.";
-  }
-
-  // Itera sobre as propriedades do objeto1
-  for (let propriedade in obj1) {
-    // Verifica se o objeto2 possui a mesma propriedade
-    if (!obj2.hasOwnProperty(propriedade)) {
-      return "Os objetos são diferentes.";
-    }
-
-    // Verifica se os valores da propriedade são iguais
-    if (obj1[propriedade] !== obj2[propriedade]) {
-      return "Os objetos são diferentes.";
-    }
-  }
-
-  // Se não houver diferenças encontradas, os objetos são iguais
-  return "Os objetos são iguais.";
-}
-
-console.log("EX 3, arquivo TypeScript \n \n", compararObjetosTs(objeto11, objeto22));
-
 //EXERCIO 4
 interface RestaurantesEx4 {
   nome: string;
@@ -140,7 +63,10 @@ const qualMelhor1 = (rest1: RestaurantesEx4, rest2: RestaurantesEx4) => {
   )}`;
 };
 
-console.log("EX 4, arquivo TypeScript \n \n", qualMelhor1(restaurantes2[0], restaurantes2[1]));
+console.log(
+  "EX 4, arquivo TypeScript \n \n",
+  qualMelhor1(restaurantes2[0], restaurantes2[1])
+);
 
 //EXERCIO 5
 interface PertoEx5 {
@@ -167,7 +93,11 @@ const pedidosPerto2: PertoEx5[] = [
   },
 ];
 
-const maisPerto1 = (pedido1: PertoEx5, pedido2: PertoEx5, pedido3: PertoEx5) => {
+const maisPerto1 = (
+  pedido1: PertoEx5,
+  pedido2: PertoEx5,
+  pedido3: PertoEx5
+) => {
   let result =
     pedido1.distancia < pedido2.distancia &&
     pedido1.distancia < pedido3.distancia
@@ -184,5 +114,4 @@ console.log(
   maisPerto1(pedidosPerto2[0], pedidosPerto2[1], pedidosPerto2[2])
 );
 
-
-const str: string[] = ['blaba', 'bla', 'bla', 'bl']
+const str: string[] = ["blaba", "claca", "dlada", "elaea"];
