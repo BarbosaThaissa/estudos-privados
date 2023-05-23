ff1a7b30d7a7399883e2c6c15c7dae9b48f3c72b
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-05-22 23:13:36 -0300

    fix: exer 3

--- testes.js
+++ testes.js
@@ -1,9 +1,3 @@
 // EXERCIO 3
 const objeto1 = { nome: "João", idade: 25 };
 const objeto2 = { nome: "Maria", idade: 30 };
-
-function compararObjetos(obj1, obj2) {
-  // Verifica se os objetos têm a mesma quantidade de propriedades
-  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-    return "Os objetos são diferentes.";
-  }
