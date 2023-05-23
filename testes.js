5f9a2fd35fe99ed6b732601d2780a75abcc214b1
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-05-22 23:13:20 -0300

    fix: exer 6

--- testes.js
+++ testes.js
@@ -7,12 +7,3 @@ function compararObjetos(obj1, obj2) {
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return "Os objetos são diferentes.";
   }
-
-  // Itera sobre as propriedades do objeto1
-  for (let propriedade in obj1) {
-    // Verifica se o objeto2 possui a mesma propriedade
-    if (!obj2.hasOwnProperty(propriedade)) {
-      return "Os objetos são diferentes.";
-    }
-
-    
