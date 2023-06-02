7ff05e8ff5060cbb62358cec9b16b61e9a108c16
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-06-01 22:50:45 -0300

    feat: alguns testes

--- testes.js
+++ testes.js
@@ -3,3 +3,8 @@ const objeto1 = { nome: "João", idade: 25 };
 const objeto2 = { nome: "Maria", idade: 30 };
 const objeto3 = { nome: "hjhj", idade: 40 };
 const objeto4 = { nome: "lll", idade: 35 };
+
+//meme
+console.log('1', 0 == '0')
+console.log('2', 0 == [])
+console.log('3', '0' == [])
