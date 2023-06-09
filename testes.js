544c652a0b92e841e27deca140931f3788bbbe6f
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-06-09 04:28:23 -0300

    feat: postar

--- testes.js
+++ testes.js
@@ -1,5 +1,1 @@
-// EXERCIO 3
-const objeto1 = { nome: "João", idade: 25 };
-const objeto2 = { nome: "Maria", idade: 30 };
-const objeto3 = { nome: "hjhj", idade: 40 };
-const objeto4 = { nome: "lll", idade: 35 };
+// Postagem
