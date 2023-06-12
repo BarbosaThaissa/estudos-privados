130f19386294f91d9c9142e57ed359379ef67164
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-06-12 03:16:22 -0300

    post

--- testes.js
+++ testes.js
@@ -36,4 +36,20 @@ myElement.classList.add("class1", "class2");
 const myInput = document.getElementById("myInput");
 const value = myInput.value;
 console.log(value);
+///
+
+const bananinha = "bananinha";
+
+console.log(bananinha.length); // Retorna o tamanho da string: 9
+console.log(bananinha.includes("na")); // Verifica se a string contém a substring "na": true
+console.log(bananinha.indexOf("ni")); // Retorna o índice da primeira ocorrência da substring "ni": 4
+console.log(bananinha.startsWith("ban")); // Verifica se a string começa com a substring "ban": true
+console.log(bananinha.endsWith("nha")); // Verifica se a string termina com a substring "nha": true
+console.log(bananinha.slice(2, 6)); // Retorna uma parte da string a partir do índice 2 até o índice 5: "nani"
+console.log(bananinha.toUpperCase()); // Converte a string para maiúsculas: "BANANINHA"
+console.log(bananinha.toLowerCase()); // Converte a string para minúsculas: "bananinha"
+console.log(bananinha.replace("na", "va")); // Substitui a primeira ocorrência da substring "na" por "va": "bavaninha"
+console.log(bananinha.repeat(3)); // Repete a string três vezes: "bananinhabananinhabananinha"
+console.log(bananinha.trim()); // Remove os espaços em branco no início e no final da string: "bananinha"
+console.log(bananinha.split("a")); // Divide a string em um array de substrings utilizando "a" como separador: ["b", "n", "ninh", ""]
 
