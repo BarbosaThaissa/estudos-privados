a8fd016903f819c47f86604e25588fe99fec31fa
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-06-17 18:23:29 -0300

    fix: arquivo teste js

--- testes.js
+++ testes.js
@@ -2,13 +2,11 @@
 
 const bananinha = "bananinha";
 
-console.log(bananinha.length); // Retorna o tamanho da string: 9
+// Retorna o índice da primeira ocorrência da substring "ni": 4
+console.log(bananinha.indexOf("ni"));
 
-console.log(bananinha.includes("na")); // Verifica se a string contém a substring "na": true
+// Verifica se a string começa com a substring "ban": true
+console.log(bananinha.startsWith("ban"));
 
-console.log(bananinha.indexOf("ni")); // Retorna o índice da primeira ocorrência da substring "ni": 4
-
-console.log(bananinha.startsWith("ban")); // Verifica se a string começa com a substring "ban": true
-
-console.log(bananinha.endsWith("nha")); // Verifica se a string termina com a substring "nha": true
-
+// Verifica se a string termina com a substring "nha": true
+console.log(bananinha.endsWith("nha"));
