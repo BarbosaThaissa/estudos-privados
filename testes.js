2dcf6ab952fa23ceee2b011fa6af7992be448104
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-06-18 22:23:18 -0300

    fix: manter dias

--- testes.js
+++ testes.js
@@ -1,12 +1,3 @@
 // Postagem
 
 const bananinha = "bananinha";
-
-// Retorna o índice da primeira ocorrência da substring "ni": 4
-console.log(bananinha.indexOf("ni"));
-
-// Verifica se a string começa com a substring "ban": true
-console.log(bananinha.startsWith("ban"));
-
-// Verifica se a string termina com a substring "nha": true
-console.log(bananinha.endsWith("nha"));
