c89b864b780714ffc64e4c0459875a740ddb0051
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-06-18 22:22:32 -0300

    fix: dia 7

--- testes.js
+++ testes.js
@@ -4,6 +4,8 @@ const bananinha = "bananinha";
 
 // Retorna o índice da primeira ocorrência da substring "ni": 4
 console.log(bananinha.indexOf("ni"));
+console.log(bananinha.indexOf("ni"));
+console.log(bananinha.indexOf("ni"));
 
 // Verifica se a string começa com a substring "ban": true
 console.log(bananinha.startsWith("ban"));
