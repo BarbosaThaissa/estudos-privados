7f199e8995bc883627de6f283d85b8991974b755
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-06-19 18:29:15 -0300

    feat: post JSON

--- testes.js
+++ testes.js
@@ -2,32 +2,14 @@
 
 //Math metodos
 
-// Math.abs(): Retorna o valor absoluto de um número.
-const num = -42;
-console.log(Math.abs(num)); // Saída: 42
+// Exemplo do método JSON.stringify()
+const obj = { name: "John", age: 30, city: "New York" };
+const jsonString = JSON.stringify(obj);
 
-// Math.ceil(): Arredonda para cima o valor de um número.
-const value = 3.14;
-console.log(Math.ceil(value)); // Saída: 4
+console.log(jsonString); // Saída: '{"name":"John","age":30,"city":"New York"}'
 
-// Math.floor(): Arredonda para baixo o valor de um número.
-const value2 = 3.99;
-console.log(Math.floor(value2)); // Saída: 3
+// Exemplo do método JSON.parse()
+const stringJson = '{"name":"John","age":30,"city":"New York"}';
+const parsedObj = JSON.parse(jsonString);
 
-// Math.max():  Retorna o maior valor entre os argumentos passados.
-const maxNumber = Math.max(10, 5, 15, 20);
-console.log(maxNumber); // Saída: 20
-
-// Math.min(): Retorna o menor valor entre os argumentos passados.
-const minNumber = Math.min(10, 5, 15, 20);
-console.log(minNumber); // Saída: 5
-
-// Math.random(): Retorna um número pseudoaleatório entre 0 e 1.
-const randomNumber = Math.random();
-console.log(randomNumber); // Saída: número pseudoaleatório entre 0 e 1
-
-// Math.sqrt(): Retorna a raiz quadrada de um número.
-const number = 16;
-const squareRoot = Math.sqrt(number);
-console.log(squareRoot); // Saída: 4
-
+console.log(parsedObj); // Saída: { name: "John", age: 30, city: "New York" }
