35009bd1b00a6c23b6b456a185afd6b16febb1b0
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-06-21 23:39:25 -0300

    fix: arquivo js

--- testes.js
+++ testes.js
@@ -5,11 +5,6 @@ const objKeys = { name: "John", age: 30, city: "New York" };
 const keys = Object.keys(objKeys);
 console.log(keys); // Saída: ["name", "age", "city"]
 
-// values(): Retorna um array contendo os valores do objeto.
-const objValues = { name: "John", age: 30, city: "New York" };
-const values = Object.values(objValues);
-console.log(values); // Saída: ["John", 30, "New York"]
-
 // entries(): Retorna um array contendo pares chave-valor do objeto.
 const objEntries = { name: "John", age: 30, city: "New York" };
 const entries = Object.entries(objEntries);
