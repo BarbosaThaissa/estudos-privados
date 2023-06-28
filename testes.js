0000d98f21447ee866df97e7361b2a0df7a07ef3
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-06-28 02:08:59 -0300

    fea: post

--- testes.js
+++ testes.js
@@ -1,9 +1,35 @@
-// keys(): Retorna um array contendo as chaves do objeto.
-const objKeys = { name: "John", age: 30, city: "New York" };
-const keys = Object.keys(objKeys);
-console.log(keys); // Saída: ["name", "age", "city"]
+// Exemplo de utilização dos métodos do objeto console
 
-// entries(): Retorna um array contendo pares chave-valor do objeto.
-const objEntries = { name: "John", age: 30, city: "New York" };
-const entries = Object.entries(objEntries);
-console.log(entries); // Saída: [["name", "John"], ["age", 30], ["city", "New York"]]
+// Limpa o console
+console.clear();
+
+// Exibe uma mensagem no console
+console.log("Hello World!");
+
+// Exibe uma mensagem de erro no console
+console.error("Mensagem de erro");
+
+// Exibe uma mensagem de aviso no console
+console.warn("Mensagem de aviso");
+
+// Exemplo de uso do console.table()
+const users = [
+  { name: "Jane", age: 25 },
+  { name: "Bob", age: 35 }
+];
+console.table(users);
+
+// Exemplo de uso do console.group()
+console.group("Informações do Usuário");
+console.log("Nome: John");
+console.log("Idade: 30");
+console.groupEnd();
+
+// Exemplo de uso do console.time()
+console.time("Tempo de execução");
+// Código a ser medido o tempo de execução
+console.timeEnd("Tempo de execução");
+
+// Exibe o rastreamento de pilha
+console.trace("Mensagem de rastreamento de pilha");
+
